e7d032eed64a570a695644c45899e6ad104a579f
jacek swoboda <jacswob@gmail.com>
2020-03-31 11:34:08 +0200

    Create changset validator for registration

---
+++ app/validations/registration.js
@@ -0,0 +1,15 @@
+import {
+  validateLength,
+  validateFormat,
+  validateConfirmation,
+  validatePresence
+} from "ember-changeset-validations/validators";
+
+export default {
+  email: validateFormat({ type: "email" }),
+  password: [validatePresence(true), validateLength({ min: 6 })],
+  repeatPassword: [
+    validatePresence(true),
+    validateConfirmation({ on: "password" })
+  ]
+};
