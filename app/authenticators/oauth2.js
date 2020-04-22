41c2220da05816ca164b636227c391636c6625a8
jacek swoboda <jacswob@gmail.com>
2020-04-22 13:45:34 +0200

    Add user authentication on reigstration

--- app/authenticators/oauth2.js
+++ app/authenticators/oauth2.js
@@ -1,5 +1,5 @@
 import OAuth2PasswordGrantAuthenticator from "ember-simple-auth/authenticators/oauth2-password-grant";
 
 export default class Oauth2Authenticator extends OAuth2PasswordGrantAuthenticator {
-  serverTokenEndpoint = "/users/sign_in";
+  serverTokenEndpoint = "http://localhost:4000/api/users/sign_in";
 }
--- app/components/registration-form.js
+++ app/components/registration-form.js
@@ -1,11 +1,19 @@
 import Component from "@glimmer/component";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class RegistrationFormComponent extends Component {
+  @service session;
+
   @action async createUser(changeset) {
     await changeset.validate();
     if (changeset.isValid) {
-      return changeset.save();
+      let model = await changeset.save();
+      return this.session.authenticate(
+        "authenticator:oauth2",
+        model.email,
+        model.password
+      );
     }
   }
 
