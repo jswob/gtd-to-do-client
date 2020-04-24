2881b11445034521d6d1a6f455ed06749d646801
jacek swoboda <jacswob@gmail.com>
2020-04-24 15:25:25 +0200

    Add instant transition after sign up

--- app/components/registration-form.js
+++ app/components/registration-form.js
@@ -3,17 +3,13 @@ import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 
 export default class RegistrationFormComponent extends Component {
-  @service session;
+  @service router;
 
   @action async createUser(changeset) {
     await changeset.validate();
     if (changeset.isValid) {
-      let model = await changeset.save();
-      return this.session.authenticate(
-        "authenticator:oauth2",
-        model.email,
-        model.password
-      );
+      await changeset.save();
+      return this.router.transtionTo("sign-in");
     }
   }
 
