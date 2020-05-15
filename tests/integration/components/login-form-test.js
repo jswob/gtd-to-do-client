5f912842512f2f36902126fde763cf40a95e2300
jacek swoboda <jacswob@gmail.com>
2020-05-15 09:59:52 +0200

    Fix eslint

--- tests/integration/components/login-form-test.js
+++ tests/integration/components/login-form-test.js
@@ -1,6 +1,6 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
-import { render, click, fillIn } from "@ember/test-helpers";
+import { render, fillIn } from "@ember/test-helpers";
 import { hbs } from "ember-cli-htmlbars";
 import Changeset from "ember-changeset";
 import LoginValidations from "gtd-to-do-client/validations/login";
