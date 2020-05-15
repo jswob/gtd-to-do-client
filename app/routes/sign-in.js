6b861d16c7a0c48e489e62d5164c6cc47603f295
jacek swoboda <jacswob@gmail.com>
2020-05-15 09:29:50 +0200

    Fix bug with multiple models after transition

--- app/routes/sign-in.js
+++ app/routes/sign-in.js
@@ -1,4 +1,12 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
+import { action } from "@ember/object";
 
 export default class SignInRoute extends Route {
+  async model() {
+    return this.store.createRecord("user", {});
+  }
+
+  @action willTransition() {
+    this.controller.model.deleteRecord();
+  }
 }
--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -1,7 +1,12 @@
 import Route from "@ember/routing/route";
+import { action } from "@ember/object";
 
 export default class SignUpRoute extends Route {
-  async model() {
+  model() {
     return this.store.createRecord("user", {});
+  }
+
+  @action willTransition() {
+    this.controller.model.deleteRecord();
   }
 }
