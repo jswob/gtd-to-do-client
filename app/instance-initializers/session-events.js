5034676522e65027b054f43133841e563eade9bd
jacek swoboda <jacswob@gmail.com>
2020-05-15 11:59:37 +0200

    Add automatic transitions after sign-in and loggin out

---
+++ app/instance-initializers/session-events.js
@@ -0,0 +1,18 @@
+export function initialize(instance) {
+  const applicationRoute = instance.__container__.lookup("route:application");
+  const session = instance.__container__.lookup("service:session");
+
+  session.on("authenticationSucceeded", function () {
+    applicationRoute.transitionTo("sign-up");
+  });
+
+  session.on("invalidationSucceeded", function () {
+    applicationRoute.transitionTo("sign-in");
+  });
+}
+
+export default {
+  initialize,
+  name: "session-events",
+  after: "ember-simple-auth",
+};
