5560c3b79b1fea12c1d58b99e54509b0486056a1
jacek swoboda <jacswob@gmail.com>
2020-05-16 12:33:46 +0200

    Set up mirage properly

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,11 @@
-'use strict';
+"use strict";
 
-module.exports = function(environment) {
+module.exports = function (environment) {
   let ENV = {
-    modulePrefix: 'gtd-to-do-client',
+    modulePrefix: "gtd-to-do-client",
     environment,
-    rootURL: '/',
-    locationType: 'auto',
+    rootURL: "/",
+    locationType: "auto",
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -13,38 +13,42 @@ module.exports = function(environment) {
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
-        Date: false
-      }
+        Date: false,
+      },
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    }
+    },
   };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+  if (environment === "development") {
+    ENV["ember-cli-mirage"] = {
+      enabled: false,
+    };
   }
 
-  if (environment === 'test') {
+  if (environment === "test") {
+    ENV["ember-cli-mirage"] = {
+      enabled: true,
+    };
+
     // Testem prefers this...
-    ENV.locationType = 'none';
+    ENV.locationType = "none";
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = "#ember-testing";
     ENV.APP.autoboot = false;
   }
 
-  if (environment === 'production') {
-    // here you can enable a production-specific feature
+  if (environment === "production") {
+    ENV["ember-cli-mirage"] = {
+      enabled: true,
+    };
   }
 
   return ENV;
