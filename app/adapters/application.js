2d361a52fbeb5ed2533db43e3a006c60b42adb4c
jswob <jacswob@gmail.com>
2020-07-31 17:56:28 +0200

    Add authorization header to every request

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,23 @@
 import RESTAdapter from "@ember-data/adapter/rest";
 import ENV from "gtd-to-do-client/config/environment";
+import DataAdapterMixin from "ember-simple-auth/mixins/data-adapter-mixin";
+import { computed } from "@ember/object";
 
-export default class ApplicationAdapter extends RESTAdapter {
+export default class ApplicationAdapter extends RESTAdapter.extend(
+  DataAdapterMixin
+) {
   host = ENV.api.host;
   namespace = ENV.api.namespace;
+
+  @computed("session.data.authenticated.token")
+  get headers() {
+    let headers = {};
+    if (this.session.isAuthenticated) {
+      headers[
+        "Authorization"
+      ] = `Bearer ${this.session.data.authenticated.access_token}`;
+    }
+
+    return headers;
+  }
 }
