c4deda833293a70225f0edbbd3e1ff5a8111ac1f
jswob <jacswob@gmail.com>
2020-09-02 17:16:51 +0200

    Fix bugs

--- app/routes/user/collections.js
+++ app/routes/user/collections.js
@@ -3,16 +3,20 @@ import { inject as service } from "@ember/service";
 import { hash } from "rsvp";
 
 export default class UserCollectionsRoute extends Route {
-  @service currentData;
+  async model() {
+    try {
+      const model = await hash({
+        buckets: await this.store.findAll("bucket"),
+        collections: await this.store.query("collection", {
+          filter: {
+            bucket_id: null,
+          },
+        }),
+      });
 
-  model() {
-    return hash({
-      buckets: this.store.findAll("bucket"),
-      collections: this.store.query("collection", {
-        filter: {
-          bucket_id: null,
-        },
-      }),
-    });
+      return model;
+    } catch (error) {
+      throw error;
+    }
   }
 }
