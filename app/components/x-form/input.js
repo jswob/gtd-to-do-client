e07876bbe9536b3edaeb5ef826a1425be22c227b
jswob <jacswob@gmail.com>
2020-09-07 17:27:57 +0200

    Add styles to input element

--- app/components/x-form/input.js
+++ app/components/x-form/input.js
@@ -8,11 +8,20 @@ export default class XFormInputComponent extends Component {
     this.inputID = `input-${randomIdentifier}`;
   }
 
+  errorClasses = "inp border-red-600 text-red-600 placeholder-red-500 w-9/12";
+  basicClasses = "inp border-gray-900 placeholder-gray-900 w-9/12";
+
+  get classes() {
+    if (this.error) return this.errorClasses;
+
+    return this.basicClasses;
+  }
+
   get error() {
     let { changeset, property } = this.args;
     if (!changeset.errors.length) return null;
 
-    let error = changeset.errors.find(error => {
+    let error = changeset.errors.find((error) => {
       if (error.key === property) {
         return true;
       }
