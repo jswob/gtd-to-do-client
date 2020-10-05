54271c9888303565fa391c67d0f7a27da4cccf87
jswob <jacswob@gmail.com>
2020-10-05 17:29:14 +0200

    Remove color from bucket component

--- app/models/bucket.js
+++ app/models/bucket.js
@@ -2,7 +2,6 @@ import Model, { attr, hasMany, belongsTo } from "@ember-data/model";
 
 export default class BucketModel extends Model {
   @attr("string") title;
-  @attr("string") color;
 
   @belongsTo("user") owner;
   @hasMany("collection") collections;
