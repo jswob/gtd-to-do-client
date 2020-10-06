ee24f10411c2338f0b3630e10ae1b3c4b24e3277
jswob <jacswob@gmail.com>
2020-10-06 18:38:31 +0200

    Create x-form/radio component

---
+++ app/components/x-form/radio.js
@@ -0,0 +1,21 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+
+export default class XFormRadioComponent extends Component {
+  get isActive() {
+    const { changeset, value, property } = this.args;
+
+    if (!changeset[property]) return false;
+
+    if (changeset[property].id == value.id) return true;
+    return false;
+  }
+
+  @action
+  async toggleActive() {
+    const { value, changeset, property } = this.args;
+
+    if (!this.isActive) changeset[property] = value;
+    else changeset[property] = null;
+  }
+}
