46a4dccff9794e243f66c304780889b310bddc49
jswob <jacswob@gmail.com>
2020-10-06 18:48:12 +0200

    Create validations for collection

---
+++ app/validations/collection.js
@@ -0,0 +1,8 @@
+import {
+  validateLength,
+  validatePresence,
+} from "ember-changeset-validations/validators";
+
+export default {
+  title: [validatePresence(true), validateLength({ min: 1, max: 35 })],
+};
