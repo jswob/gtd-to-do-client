92f2b6d636a5532e74395b9577d33f7a1ac7b686
jswob <jacswob@gmail.com>
2020-10-06 18:49:04 +0200

    Implement collection-form component

---
+++ app/controllers/user/collections/new.js
@@ -0,0 +1,6 @@
+import Controller from "@ember/controller";
+import CollectionValidations from "../../../validations/collection";
+
+export default class UserCollectionsNewController extends Controller {
+  CollectionValidations = CollectionValidations;
+}
--- app/routes/user/collections/new.js
+++ app/routes/user/collections/new.js
@@ -1,4 +1,16 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
+import { action } from "@ember/object";
+import { hash } from "rsvp";
 
 export default class UserCollectionsNewRoute extends Route {
+  async model() {
+    return hash({
+      collection: this.store.createRecord("collection", {}),
+      buckets: this.modelFor("user.collections").buckets,
+    });
+  }
+
+  @action willTransition() {
+    this.controller.model.collection.deleteRecord();
+  }
 }
