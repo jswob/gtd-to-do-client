5634ed78e92dc8a376cc3b9c9fe913f03597d40b
jswob <jacswob@gmail.com>
2020-10-06 15:04:51 +0200

    Implement delete-model-form

--- app/routes/user/bucket/delete.js
+++ app/routes/user/bucket/delete.js
@@ -1,4 +1,7 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
 
 export default class UserBucketDeleteRoute extends Route {
+  model() {
+    return this.modelFor("user.bucket");
+  }
 }
