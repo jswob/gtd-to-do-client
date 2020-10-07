5d5edc046847ccbdf97e4da36e3c0786c012055c
jswob <jacswob@gmail.com>
2020-10-07 20:07:47 +0200

    Set up collection/edit complexity

---
+++ app/controllers/user/collections/collection/edit.js
@@ -0,0 +1,6 @@
+import Controller from "@ember/controller";
+import CollectionValidations from "../../../../validations/collection";
+
+export default class UserCollectionsCollectionEditController extends Controller {
+  CollectionValidations = CollectionValidations;
+}
--- app/routes/user/collections/collection/edit.js
+++ app/routes/user/collections/collection/edit.js
@@ -1,4 +1,23 @@
-import Route from '@ember/routing/route';
+import Route from "@ember/routing/route";
+import { hash } from "rsvp";
 
 export default class UserCollectionsCollectionEditRoute extends Route {
+  async model() {
+    const buckets = await this.modelFor("user.collections").buckets;
+    const collection = await this.modelFor("user.collections.collection");
+    const bucket = await collection.bucket;
+
+    if (bucket) {
+      const currentBucket = await this.store.findRecord(
+        "bucket",
+        bucket.get("id")
+      );
+      await buckets.toArray().pushObject(currentBucket);
+    }
+
+    return hash({
+      collection: collection,
+      buckets: buckets,
+    });
+  }
 }
