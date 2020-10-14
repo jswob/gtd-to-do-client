5e66696ec3b9d2719eaf56922079544ab43ba5b7
jswob <jacswob@gmail.com>
2020-10-14 13:06:00 +0200

    Fix tests for x-form/radio

--- tests/integration/components/x-form/radio-test.js
+++ tests/integration/components/x-form/radio-test.js
@@ -1,108 +1,103 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { render, click } from "@ember/test-helpers";
+import Changeset from "ember-changeset";
+import CollectionValidations from "gtd-to-do-client/validations/collection";
+import lookupValidator from "ember-changeset-validations";
 import { hbs } from "ember-cli-htmlbars";
 import { A } from "@ember/array";
 
 module("Integration | Component | x-form/radio", function (hooks) {
   setupRenderingTest(hooks);
 
-  // Test doesn't work for some obscure reason but i will find out why
-
-  // test("it managa isActive correctly", async function (assert) {
-  //   const property = "property";
-  //   const objects = A([
-  //     {
-  //       id: 1,
-  //       title: "test-1",
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "test-2",
-  //     },
-  //   ]);
-  //   const changeset = {
-  //     id: 1,
-  //     property: objects[0],
-  //   };
-  //   this.set("property", property);
-  //   this.set("objects", objects);
-  //   this.set("changeset", changeset);
-
-  //   const activeClass = "active";
-  //   this.set("activeClass", activeClass);
-
-  //   await render(hbs`
-  //   {{#each this.objects as |object|}}
-  //     <XForm::Radio data-test-button="{{object.id}}" @label={{object.title}} @value={{object}}
-  //       @changeset={{this.changeset}} @property={{this.property}}
-  //       @activeClass={{this.activeClass}} />
-  //   {{/each}}
-  //   `);
-
-  //   assert.dom(`[data-test-button='${objects[0].id}']`).hasClass(activeClass);
-  //   assert
-  //     .dom(`[data-test-button='${objects[1].id}']`)
-  //     .doesNotHaveClass(activeClass);
-
-  //   await click(`[data-test-button='${objects[1].id}']`);
-
-  //   assert
-  //     .dom(`[data-test-button='${objects[0].id}']`)
-  //     .doesNotHaveClass(activeClass);
-  //   assert.dom(`[data-test-button='${objects[1].id}']`).hasClass(activeClass);
-
-  //   await click(`[data-test-button='${objects[1].id}']`);
-
-  //   assert
-  //     .dom(`[data-test-button='${objects[0].id}']`)
-  //     .doesNotHaveClass(activeClass);
-  //   assert
-  //     .dom(`[data-test-button='${objects[1].id}']`)
-  //     .doesNotHaveClass(activeClass);
-  //   await this.pauseTest();
-  // });
-
-  test("it manage changeset correctly", async function (assert) {
-    const property = "property";
-    const objects = A([
-      {
-        id: 1,
-        title: "test-1",
-      },
-      {
-        id: 2,
-        title: "test-2",
-      },
+  hooks.beforeEach(function () {
+    const store = this.owner.lookup("service:store");
+    const buckets = A([
+      store.createRecord("bucket", { id: 0, title: "some_b_0" }),
+      store.createRecord("bucket", { id: 1, title: "some_b_1" }),
     ]);
-    const changeset = {
-      id: 1,
-      property: objects[0],
-    };
 
-    this.set("property", property);
-    this.set("objects", objects);
+    const collection = store.createRecord("collection", {
+      id: 0,
+      title: "some_c_0",
+      bucket: buckets.firstObject,
+    });
+
+    const changeset = new Changeset(
+      collection,
+      lookupValidator(CollectionValidations)
+    );
+
+    this.set("buckets", buckets);
     this.set("changeset", changeset);
+    this.set("property", "bucket");
+  });
 
+  test("it manage isActive correctly", async function (assert) {
     const activeClass = "active";
     this.set("activeClass", activeClass);
 
     await render(hbs`
-    {{#each this.objects as |object|}}
-      <XForm::Radio data-test-button="{{object.id}}" @label={{object.title}} @value={{object}}
+    {{#each this.buckets as |bucket|}}
+      <XForm::Radio data-test-button="{{bucket.id}}" @label={{bucket.title}} @value={{bucket}}
         @changeset={{this.changeset}} @property={{this.property}}
         @activeClass={{this.activeClass}} />
     {{/each}}
     `);
 
-    assert.equal(changeset[property].id, 1, "First option is active");
+    assert
+      .dom(`[data-test-button='${this.get("buckets")[0].id}']`)
+      .hasClass(activeClass);
+    assert
+      .dom(`[data-test-button='${this.get("buckets")[1].id}']`)
+      .doesNotHaveClass(activeClass);
 
-    await click(`[data-test-button='${objects[1].id}']`);
+    await click(`[data-test-button='${this.get("buckets")[1].id}']`);
 
-    assert.equal(changeset[property].id, 2, "Second option is active");
+    assert
+      .dom(`[data-test-button='${this.get("buckets")[0].id}']`)
+      .doesNotHaveClass(activeClass);
+    assert
+      .dom(`[data-test-button='${this.get("buckets")[1].id}']`)
+      .hasClass(activeClass);
 
-    await click(`[data-test-button='${objects[1].id}']`);
+    await click(`[data-test-button='${this.get("buckets")[1].id}']`);
 
-    assert.notOk(changeset[property], "None option is active");
+    assert
+      .dom(`[data-test-button='${this.get("buckets")[0].id}']`)
+      .doesNotHaveClass(activeClass);
+    assert
+      .dom(`[data-test-button='${this.get("buckets")[1].id}']`)
+      .doesNotHaveClass(activeClass);
+  });
+
+  test("it manage changeset correctly", async function (assert) {
+    await render(hbs`
+    {{#each this.buckets as |bucket|}}
+      <XForm::Radio data-test-button="{{bucket.id}}" @label={{bucket.title}} @value={{bucket}}
+        @changeset={{this.changeset}} @property={{this.property}} />
+    {{/each}}
+    `);
+
+    assert.equal(
+      this.get(`changeset.${this.get("property")}.id`),
+      0,
+      "First option is active"
+    );
+
+    await click(`[data-test-button='${this.get("buckets")[1].get("id")}']`);
+
+    assert.equal(
+      this.get(`changeset.${this.get("property")}.id`),
+      1,
+      "Second option is active"
+    );
+
+    await click(`[data-test-button='${this.get("buckets")[1].get("id")}']`);
+
+    assert.notOk(
+      this.get(`changeset.${this.get("property")}.id`),
+      "None option is active"
+    );
   });
 });
