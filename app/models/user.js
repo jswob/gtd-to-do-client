388ac4109fb60afb7a849097354817ed7798e777
jswob <jacswob@gmail.com>
2020-10-17 15:44:40 +0200

    Implement profile-menu component

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,7 @@ import Model, { attr } from "@ember-data/model";
 
 export default class UserModel extends Model {
   @attr("string") email;
+  @attr("string") avatarUrl;
   @attr("string") password;
   @attr("string") repeatPassword;
 }
