909c42856caef5d5c9ddaf57b58fcd2766ccab32
jswob <jacswob@gmail.com>
2020-10-19 17:05:53 +0200

    Create edit-user-main component

---
+++ app/components/edit-user-main.js
@@ -0,0 +1,6 @@
+import Component from "@glimmer/component";
+import EditUserValidations from "gtd-to-do-client/validations/edit-user";
+
+export default class EditUserMainComponent extends Component {
+  EditUserValidations = EditUserValidations;
+}
