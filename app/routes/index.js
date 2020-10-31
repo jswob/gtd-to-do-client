40bfe2d97e2d3a48a1ef3de06daff832dab9f702
jswob <jacswob@gmail.com>
2020-10-31 12:45:28 +0100

    Fix bugs in index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Route from "@ember/routing/route";
 
 export default class IndexRoute extends Route {
-  // beforeModel() {
-  //   this.transitionTo("sign-in");
-  // }
+  beforeModel() {
+    this.transitionTo("sign-in");
+  }
 }
